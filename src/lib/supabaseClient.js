b244916986ad9188c240cd1bf8317a70311e830a
Rae Triadi <jkotriady@gmail.com>
2026-06-16 19:39:31 +0700

    fix: bypass env issue by hardcoding supabase client

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://pcwjdtckpepmyruwmpfe.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBjd2pkdGNrcGVwbXlydXdtcGZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExODEwMTAsImV4cCI6MjA5Njc1NzAxMH0.3zmY5wd0i43HBFdAuD2Q4mPufasKEF_ckrpMaQuVpmE'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
